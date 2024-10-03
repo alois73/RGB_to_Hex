6e8f9f6e4b456c8795823b6a689681bff713d834
Alois Mucaj <54184896+alois73@users.noreply.github.com>
2024-10-03 12:01:16 +0200

    user input

--- rgb_to_hex.py
+++ rgb_to_hex.py
@@ -1,6 +1,6 @@
 import math
 
-def nums(a):
+def split(a):
     if a > 255:
         a = 255
     if a < 0:
@@ -14,9 +14,9 @@ def nums(a):
     return a1, a2
 
 def rgb(r, g, b):
-    r1, r2 = nums(r)
-    g1, g2 = nums(g)
-    b1, b2 = nums(b)
+    r1, r2 = split(r)
+    g1, g2 = split(g)
+    b1, b2 = split(b)
 
     hex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']         
     new_r = hex[r1] + hex[r2]
@@ -24,3 +24,14 @@ def rgb(r, g, b):
     new_b = hex[b1] + hex[b2]
     rgb_to_hex = new_r + "" + new_g + "" + new_b
     return rgb_to_hex
+
+def use():
+    print("RGB")
+    print(" ")
+    r = int(input("R: "))
+    g = int(input("G: "))
+    b = int(input("B: "))
+    print(" ")
+    print("HEX: ", rgb(r, g, b))
+
+use()
